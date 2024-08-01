6559f4a21d158864302f3493c32fc8370b57fab9
Eli Keli <muthokaeli@gmail.com>
2024-08-01 23:59:12 +0300

    Kijani Kilimani landing page - change

--- green-space/src/components/UpcomingEvents.jsx
+++ green-space/src/components/UpcomingEvents.jsx
@@ -1,38 +1,47 @@
 import "../styles/UpcomingEvents.css"
+import yoga from "../assets/images/yoga.png"
+import events from "../assets/images/events.png"
+import gardening from "../assets/images/gardening.png"
 
 const UpcomingEvents = () => {
   return (
     <section>
-            <div class="upcoming-events-container">
+            <div className="upcoming-events-container">
                 <h2>Upcoming events</h2>
-                <div class="upcoming-event">
+                <div className="upcoming-event">
                     <div>
                        <p>Free</p> 
                        <h4>Gardening 101</h4>
                        <p>Saturday, May 20th 10:00 AM - 12:00 PM</p>
-                       <button class="btn">sign up now</button>
+                       <button className="btn">sign up now</button>
                     </div>
-                    {/* <img> */}
+                    <div className="event-img">
+                        <img src={gardening} alt="" />
+                    </div>
                 </div>
-                <div class="upcoming-event">
+                <div className="upcoming-event">
                     <div>
                        <p>Free</p> 
                        <h4>Yoga in the park</h4>
                        <p>Saturday, May 20th 10:00 AM - 12:00 PM</p>
-                       <button class="btn">sign up now</button>
+                       <button className="btn">sign up now</button>
                     </div>
-                    {/* <img> */}
+                    <div className="event-img">
+                        <img src={yoga} alt="" />
+                    </div>
                 </div>
-                <div class="upcoming-event">
+                <div className="upcoming-event">
                     <div>
                        <p>Free</p> 
-                       <h4>Gardening 101</h4>
+                       <h4>Community picnic</h4>
                        <p>Saturday, May 20th 10:00 AM - 12:00 PM</p>
-                       <button class="btn">sign up now</button>
+                       <button className="btn">sign up now</button>
                     </div>
-                    {/* <img> */}
+                    <div className="event-img">
+                        <img src={events} alt="" />
+                    </div>
                 </div>
-                <div class="end"><button class="btn btn-secondary">View all Events</button></div>
+                <div className="end"><button className="btn btn-secondary">View all Events</button></div>
             </div>
         </section>
   )
--- green-space/src/components/WhatWeOffer.jsx
+++ green-space/src/components/WhatWeOffer.jsx
@@ -1,26 +1,50 @@
 import "../styles/WhatWeOffer.css"
+import yoga from "../assets/images/yoga.png"
+import events from "../assets/images/events.png"
+import recreation from "../assets/images/recreation.png"
+import picnic from "../assets/images/picnic.png"
 
 const WhatWeOffer = () => {
     return (
         <section>
-            <div className="what-we-offer-container">
+            <div class="what-we-offer-container">
                 <h2>What we offer</h2>
-                <div className="flexbox">
-                    <div>
-                        <h3>Community Events</h3>
-                        <p>Join us for a variety of fun and educational events</p>
+                <div class="flexbox">
+                    <div class="flex">
+                        <div class="offer-img">
+                            <img src={events} alt="" />
+                        </div>
+                        <div>
+                            <h3>Community Events</h3>
+                            <p>Join us for a variety of fun and educational events</p>
+                        </div>
                     </div>
-                    <div>
-                        <h3>Recreation</h3>
-                        <p>We offer a wide range of recreation opportunities</p>
+                    <div class="flex">
+                        <div class="offer-img">
+                            <img src={recreation} alt="" />
+                        </div>
+                        <div>
+                            <h3>Recreation</h3>
+                            <p>We offer a wide range of recreation opportunities</p>
+                        </div>
                     </div>
-                    <div>
-                        <h3>Picnic Areas</h3>
-                        <p>Bring your friends and family and enjoy a picnic</p>
+                    <div class="flex">
+                        <div class="offer-img">
+                            <img src={picnic} alt="" />
+                        </div>
+                        <div>
+                            <h3>Picnic Areas</h3>
+                            <p>Bring your friends and family and enjoy a picnic</p>
+                        </div>
                     </div>
-                    <div>
-                        <h3>Yoga classes</h3>
-                        <p>Relax and rejuvenate with our outdoor yoga classes</p>
+                    <div class="flex">
+                        <div class="offer-img">
+                            <img src={yoga} alt="" />
+                        </div>
+                        <div>
+                            <h3>Yoga classes</h3>
+                            <p>Relax and rejuvenate with our outdoor yoga classes</p>
+                        </div>
                     </div>
                 </div>
             </div>
